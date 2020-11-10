c70d99650d15586146f8bc9101d82eb06e14db0f
berkatil <atilberk98@gmail.com>
2020-11-10 10:35:44 +0200

    Initial commit

---
+++ Character.cpp
@@ -0,0 +1,21 @@
+#include "Character.h"
+
+Character::Character(string _name, string _type, int _attack, int _defense, int _remainingHealth, int _nMaxRounds) {
+
+}
+
+Character::Character(const Character& character) {
+
+}
+
+Character Character::operator=(const Character& character) {
+
+}
+
+bool Character::operator<(const Character& other) {
+
+}
+
+Character::~Character() {
+
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "Character.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}

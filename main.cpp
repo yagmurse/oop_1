f37c4c63f3a283f751b4384bdaf6dffef0c464d7
yagmurse <52867310+yagmurse@users.noreply.github.com>
2020-11-27 22:12:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,22 +20,22 @@ int currentRound;
 int elves_special=10;
 int dwarfs_special=20;
 int wizards_special=50;
-vector<Character> community_1;
-vector<Character> community_2;
-queue < pair < pair < string,string > ,string > > roundInfo; //pair<(attacker_name,defender_name),Ifspecial>
+vector<Character> community_1; //holds Characters for community 1
+vector<Character> community_2; //holds Characters for community2
+queue < pair < pair < string,string > ,string > > roundInfo; //pair<(attacker_name,defender_name),Ifspecial>, round informations given
 void special_wizards(vector<Character> &community,Character &character);
 void special_elves(vector<Character> &community,Character &chr);
 bool special_dwarfs(Character &character);
-bool is_war_end(ofstream& outFile);
-void one_round(Character &attacker,Character &defender,vector<Character> &teamA, vector<Character> &teamD,string &ifSpecial);
-void war_begin(ofstream& outFile);
-bool turn_1;
-bool sort_for_char(Character &c1,Character &c2);
+bool is_war_end(ofstream& outFile); //check if one is the winner
+void one_round(Character &attacker,Character &defender,vector<Character> &teamA, vector<Character> &teamD,string &ifSpecial); //play 1 round
+void war_begin(ofstream& outFile); 
+bool turn_1; //does first community attacks, is it first's turn
+bool sort_for_char(Character &c1,Character &c2);  
 vector<Character> sort_alph_order(vector<Character> &vec);
 
-stack<Character> lastKilled_1;
-stack<Character> lastKilled_2;
-void deneme_print(ofstream& outFile, queue<string> com1, queue<string> com2);
+stack<Character> lastKilled_1; //last killed chars for community1
+stack<Character> lastKilled_2; //last killed chars for community2
+void deneme_print(ofstream& outFile, queue<string> com1, queue<string> com2); //prints the output to the out file
 
 
 
@@ -67,7 +67,7 @@ int main(int argc, char* argv[]) {
     vector< vector< string > > all_input;
     string input;
     int currentlineNumber=0;
-    // ifstream inputfile ("/Users/yselek14/Desktop/project1-yagmurse-master/testcases/inputs/input_7.txt");
+   
     //store input line by line to global vector all_input
     if (inputFile.is_open())
     {
@@ -93,7 +93,8 @@ int main(int argc, char* argv[]) {
         inputFile.close();
     }
     else { cout << "Unable to open input file"; }
-    //place_inputs();
+    
+    //place inputs into communities
     for(int i=0;i<10;i++) {
         vector<string> tmp=all_input[i];
         Character tmpChar=*new Character(tmp[0],tmp[1],stoi(tmp[2]),stoi(tmp[3]),stoi(tmp[4]),maxRoundGiven);
@@ -106,6 +107,7 @@ int main(int argc, char* argv[]) {
             com_2_given_order.push(tmp[0]);
         }
     }
+    
     //record attacks into attackInfo
     int remaining=all_input.size();
     for(int i=10;i<remaining;i++) {
@@ -113,18 +115,17 @@ int main(int argc, char* argv[]) {
         pair<pair<string,string>,string> round=make_pair(competitors,all_input[i][2]);
         roundInfo.push(round);
     }
+    //sort communities in alphabetical order
     community_1=sort_alph_order(community_1);
     community_2=sort_alph_order(community_2);
 
 
-
-    war_begin(outputFile);
-    deneme_print(outputFile,com_1_given_order,com_2_given_order);
+    war_begin(outputFile); //begin the war
+    deneme_print(outputFile,com_1_given_order,com_2_given_order); //print the result to the outfile
     return 0;
 }
 
 void war_begin(ofstream& outFile) {
-    //bool _is_war_end;
     currentRound=0;
     for(int i=0;i<5;i++) {
         community_1[i].healthHistory[0]=community_1[i].remainingHealth;
@@ -140,8 +141,8 @@ void war_begin(ofstream& outFile) {
         string attackerStr=currentPair.first.first;
         string defenderStr=currentPair.first.second;
         string special=currentPair.second;
-        int com1int=0;
-        int com2int=0;
+        int com1int=0;  //decides the player index for community 1
+        int com2int=0;  //decides the player index for community 2
         for(int i=0;i<5;i++) {
             if(com1int != 0 && com2int != 0)
                 break;
@@ -154,6 +155,7 @@ void war_begin(ofstream& outFile) {
             if(community_2[i].name==defenderStr)
                 com2int=i;
         }
+        //change the player if given player is not alive for community 1 and 2 respectively
         if(!community_1[com1int].isAlive) {
             for(int i=1;i<5;i++) {
                 int next=com1int+i;
@@ -175,9 +177,11 @@ void war_begin(ofstream& outFile) {
             }
         }
         if(turn_1) {
+            //if community 1 attacks
             one_round(community_1[com1int],community_2[com2int],community_1,community_2,special);
         }
         else {
+            //if community 2 attacks
             one_round(community_2[com2int],community_1[com1int],community_2,community_1,special);
         }
     }
@@ -218,6 +222,7 @@ void one_round(Character &attacker, Character &defender, vector<Character> &team
     }
 
 }
+//Elves convey half of its life to Hobbits if conditions satisfied
 void special_elves(vector<Character> &community,Character &chr) {
     if(chr.type=="Elves" && chr.nRoundsSinceSpecial>=elves_special) {
         chr.nRoundsSinceSpecial=0;
@@ -234,6 +239,7 @@ void special_elves(vector<Character> &community,Character &chr) {
         }
     }
 }
+
 bool special_dwarfs(Character &character) {
     if(character.type=="Dwarfs" && (character.nRoundsSinceSpecial>=dwarfs_special)) {
         character.nRoundsSinceSpecial=0;
@@ -246,6 +252,7 @@ bool special_dwarfs(Character &character) {
     }
 }
 
+//wizard gives the initial health for lastly killed member of its community 
 void special_wizards(vector<Character> &community,Character &character) {
     if(character.type=="Wizards" && character.nRoundsSinceSpecial>=wizards_special ) {
         string lastDied="";
@@ -270,6 +277,7 @@ void special_wizards(vector<Character> &community,Character &character) {
     }
 }
 
+//check if hobbit is death or all other members are death for each community 
 bool is_war_end(ofstream& outFile) {
     bool result=false;
     int deathNotHobbit1=0;
@@ -336,6 +344,7 @@ vector<Character> sort_alph_order(vector<Character> &vec){
 }
 
 void deneme_print(ofstream& outFile,queue<string> com1, queue<string> com2) {
+    //do modifications to print the output in given name order and print
     for(int i=0;i<5;i++) {
         string s=com1.front();
         com1.pop();
